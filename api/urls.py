c14e354d1d9f1353328277b7315c5f6b69245cf5
Rodrigue Roland CODO <codorol22@gmail.com>
2022-12-21 23:42:23 +0500

    Api overview at root

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from rest_framework.schemas import get_schema_view
 
 from .views import EntityList
 
+schema_view = get_schema_view(
+    title='API title',
+    description='API description',
+    version='1.0.0',
+)
+
 urlpatterns = [
+    path('', schema_view, name='openapi-schema'),
     path('entities/', EntityList.as_view()),
 ]
